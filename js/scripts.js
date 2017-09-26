ce917ad9e9e4e9c933802caec74e77235594eaac
Krzych <krzych.pierzchala@gmail.com>
2017-09-26 17:37:19 +0200

    Removed link to css from html file and wrote function for triangle area in js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function getTriangleArea(a, h)
+{
+	if ((a < 0) || (h < 0)) return 'Nieprawidłowe dane'
+		return 1/2 * a * h
+}
+
+console.log(getTriangleArea(10, 6));
+var triangleArea1 = getTriangleArea(25, 10.5);
+var triangleArea2 = getTriangleArea(5, 0);
+var triangleArea3 = getTriangleArea(-30, 5);
+var triangleArea4 = getTriangleArea(10, 20);
+
+console.log(triangleArea1);
+console.log(triangleArea2);
+console.log(triangleArea3);
+console.log(triangleArea4);
